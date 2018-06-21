c8935e0007ad125f2b5542e776612751d5285079
Nirav Patel <nrvpatel03@gmail.com>
2018-06-21 09:09:45 -0700

    it works

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,14 @@
 require("dotenv").config()
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
 var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'pizzas_db'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/pizzaController");
+var routes = require("./controllers/pizzaController.js");
 //routes
 app.use(routes);
 
